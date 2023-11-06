7e7816be437b4f47c196b2b0fbc474f8a530ea0b
saibattula93 <saidurgaprasadbattula@gmail.com>
2023-11-06 16:00:10 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Stock-Image-Identification",
+    version='0.0.0',
+    author='Sai Durga Prasad',
+    author_email="saidurgaprasadbattula@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
